d6fea93912903f15e633bed6f3a541f7a79de2c0
Toby Zerner <toby.zerner@gmail.com>
2019-11-21 15:14:59 +1030

    Encode query parameters according to RFC 3986

--- dist/query.d.ts
+++ dist/query.d.ts
@@ -2,6 +2,5 @@ export declare class Query {
     private readonly query;
     constructor(query?: any);
     push(key: object | string, value?: any): this;
-    private appendable;
     toString(): string;
 }
--- src/query.ts
+++ src/query.ts
@@ -1,3 +1,13 @@
+function fixedEncodeURIComponent(str: string): string {
+    return encodeURIComponent(str).replace(/[!'()*]/g, function(c) {
+        return '%' + c.charCodeAt(0).toString(16).toUpperCase();
+    });
+}
+
+function appendable(key: string): boolean {
+    return key === 'include' || /fields\[[^\]]+]/i.test(key);
+}
+
 export class Query {
     private readonly query: any;
 
@@ -8,7 +18,7 @@ export class Query {
     public push(key: object | string, value?: any): this {
         if (typeof key === 'object') {
             Object.entries(key).map(entry => this.push.apply(this, entry));
-        } else if (this.appendable(key)) {
+        } else if (appendable(key)) {
             this.query[key] = (this.query[key] ? this.query[key] + ',' : '') + value;
         } else {
             this.query[key] = value;
@@ -17,14 +27,10 @@ export class Query {
         return this;
     }
 
-    private appendable(key: string): boolean {
-        return key === 'include' || /fields\[[^\]]+]/i.test(key);
-    }
-
     public toString(): string {
         return Object.entries<any>(this.query)
             .sort((a, b) => a[0].localeCompare(b[0]))
-            .map(([k, v]) => encodeURIComponent(k) + '=' + encodeURIComponent(v))
+            .map(([k, v]) => fixedEncodeURIComponent(k) + '=' + fixedEncodeURIComponent(v))
             .join('&');
     }
 }
