d7dfbb98aad3a488a967941b5de78c5ed148ee38
Toby Zerner <toby.zerner@gmail.com>
2021-01-17 16:20:51 +1030

    Update rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,5 @@
 import pkg from './package.json';
-import typescript from 'rollup-plugin-typescript2';
+import typescript from '@rollup/plugin-typescript';
 import { terser } from 'rollup-plugin-terser';
 
 export default {
